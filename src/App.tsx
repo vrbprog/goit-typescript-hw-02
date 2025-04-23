3907be042f5bc11df4b47a57c7dbd210b9cca58f
vrbprog <vrbdsp@gmail.com>
2025-04-23 19:50:55 +0300

    Add App code

--- src/App.tsx
+++ src/App.tsx
@@ -8,80 +8,69 @@ import Loader from "./components/Loader/Loader";
 import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
 import ImageModal from "./components/ImageModal/ImageModal";
 
-// type Image = {
-//     id: string;
-//     urls: {
-//         small: string;
-//         regular: string;
-//     };
-//     alt_description: string;
-// };
-
-// type Images = {
-//     images: Image[];
-//     onOpenModal: (src: string, alt: string) => void;
-// };
+interface UnsplashPhoto {
+    id: string;
+    alt_description: string;
+    urls: {
+        small: string;
+        regular: string;
+    };
+}
 
 export default function App() {
-    
-    const [query, setQuery] = useState('');
+    const [query, setQuery] = useState("");
     const [page, setPage] = useState(1);
-    const [articles, setArticles] = useState([]);
+    const [articles, setArticles] = useState<UnsplashPhoto[]>([]);
     const [isLoading, setIsLoading] = useState(false);
     const [isError, setIsError] = useState(false);
     const [isNotLastPage, setIsNotLastPage] = useState(false);
     const [modalIsOpen, setModalIsOpen] = useState(false);
-    const [modalImage, setModalImage] = useState('');
-    const [modalDescription, setModalDescription] = useState('');
+    const [modalImage, setModalImage] = useState("");
+    const [modalDescription, setModalDescription] = useState("");
 
     useEffect(() => {
-
         const getArticlesData = async () => {
-        try {
-            
-            if (query === '') return;
-            
-            setIsError(false);
-            setIsLoading(true);
-            const { total_pages, results } = await fetchImages(query, page);
-            if(page === total_pages) {
-                setIsNotLastPage(false);
-            }
-            else {
-                setIsNotLastPage(true);
-            }
+            try {
+                if (query === "") return;
 
-            if (total_pages === 0) {
-                toast.error('No results were found for your query!');
-                setArticles([]);
-            }
-            else {
-                if (page === 1) {
-                    setArticles(results);
+                setIsError(false);
+                setIsLoading(true);
+                const { total_pages, results } = await fetchImages(query, page);
+                if (page === total_pages) {
+                    setIsNotLastPage(false);
+                } else {
+                    setIsNotLastPage(true);
                 }
-                else {
-                    setArticles((art) => [...art, ...results]);
+
+                if (total_pages === 0) {
+                    toast.error("No results were found for your query!");
+                    setArticles([]);
+                } else {
+                    if (page === 1) {
+                        setArticles(results);
+                    } else {
+                        setArticles((art) => [...art, ...results]);
+                    }
                 }
+            } catch {
+                setIsError(true);
+            } finally {
+                setIsLoading(false);
             }
-        } catch {
-            setIsError(true);
-        } finally {
-            setIsLoading(false);
-        }
         };
-        
-    getArticlesData();
+
+        getArticlesData();
     }, [query, page]);
 
     const request = (newQuery: string) => {
-        if(query === newQuery) return;
+        if (query === newQuery) return;
         setQuery(newQuery);
         setPage(1);
     };
 
     const isLoadImages = () => {
         return articles.length > 0;
-    }
+    };
 
     const handleLoadMore = () => {
         setPage((page) => page + 1);
@@ -91,21 +80,27 @@ export default function App() {
         setModalImage(image);
         setModalDescription(description);
         setModalIsOpen(true);
-    }
+    };
 
     return (
         <>
             <SearchBar request={request} />
 
-            {isError ? <ErrorMessage /> :
+            {isError ? (
+                <ErrorMessage />
+            ) : (
                 <>
-                    <ImageGallery images={articles} onOpenModal={handleModal}/>
-                    {isLoading ? <Loader /> :
-                        isNotLastPage && isLoadImages() &&
-                        <LoadMoreBtn onLoadMore={handleLoadMore} />
-                    }
+                    <ImageGallery images={articles} onOpenModal={handleModal} />
+                    {isLoading ? (
+                        <Loader />
+                    ) : (
+                        isNotLastPage &&
+                        isLoadImages() && (
+                            <LoadMoreBtn onLoadMore={handleLoadMore} />
+                        )
+                    )}
                 </>
-            }
+            )}
 
             <ImageModal
                 isOpen={modalIsOpen}
--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,16 +1,18 @@
-import { RotatingLines } from "react-loader-spinner";
+// import { RotatingLines } from "react-loader-spinner";
+//import Loader  from "react-ts-loaders";
 import css from "./Loader.module.css";
 
 export default function Loader() {
     return (
-      <div className={css.container}>
-        <RotatingLines
+        <div className={css.container}>
+            {/* <RotatingLines
             strokeColor="blue"
             strokeWidth="5"
             animationDuration="0.75"
             width="96"
             visible={true}
-        />
-    </div>
-  )
+        /> */}
+            Loading...
+        </div>
+    );
 }
